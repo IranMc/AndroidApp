0202f22da9a82338978314bfd25ec67ce3a69bdb
Erfan <113671298+ErfanAzadi@users.noreply.github.com>
2023-09-10 14:08:40 +0330

    Update NewsList.js

--- NewsList.js
+++ NewsList.js
@@ -1,12 +1,22 @@
 [
- {
-            "title": "ممنک ارش",
-            "content": "ممنگ ارش رو تپلیدم",
-            "tag": "اعلان",
-            "writer": "Smth",
-            "img": "https://cdn.manzoom.ir/media/person_images/d6ahVx0bWMSoJhhS4C-5x2Rjt54QKXx5GBCAThgQgE4.jpg.215x307_q100_box-69%2C0%2C1737%2C2382_crop_detail.jpg",
-            "date": "فاحشه خانه",
-            "time_stamp": 293483948,
-            "id": 42
-        }
+    {
+        "title": "ممنک ارش",
+        "content": "ممنگ ارش رو تپلیدم",
+        "tag": "اعلان",
+        "writer": "Smth",
+        "img": "https://cdn.manzoom.ir/media/person_images/d6ahVx0bWMSoJhhS4C-5x2Rjt54QKXx5GBCAThgQgE4.jpg.215x307_q100_box-69%2C0%2C1737%2C2382_crop_detail.jpg",
+        "date": "فاحشه خانه",
+        "time_stamp": 293483948,
+        "id": 42
+    },
+    {
+        "title": "ددنگ آرش ارش",
+        "content": "ددنگ ارش رو تپلیدم",
+        "tag": "اعلان",
+        "writer": "Smth",
+        "img": "https://media.isna.ir/oldcontent?th=2&n=pics/1383/4/Photo/267.jpg",
+        "date": "فاحشه خانه",
+        "time_stamp": 293483948,
+        "id": 43
+    }
 ]
